6fc6d3f4e8d7add2e7c006ac4f50c39cf9aa9543
Mahdi Nikdan <nikdanmahdi@gmail.com>
2024-02-12 07:56:23 +0100

    Update restart_7b_viggo_4bit.sh

--- scripts/train/scripts/llama2-7b/restart_7b_viggo_4bit.sh
+++ scripts/train/scripts/llama2-7b/restart_7b_viggo_4bit.sh
@@ -2,8 +2,8 @@ bash scripts/restarter_llama2.sh \
     MODEL_SIZE=7b \
     MODEL_PRECISION=4bit \
     DATASET=viggo \
-    LR=0.0003 \
-    LORA_LR=0.0007 \
+    LR=0.0001 \
+    LORA_LR=0.001 \
     SPA_DENSITY=0.009 \
     LORA_R=8 \
     NUM_EPOCHS=1
